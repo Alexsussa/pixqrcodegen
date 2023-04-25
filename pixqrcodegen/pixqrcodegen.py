6f70a1daaa8348aea791021516fe5df3bd770ddb
Alvaro Figueiredo <alvaroaf+git@gmail.com>
2023-04-24 21:25:29 -0300

    Use native string format to ensure the zero digit in front of the length fields

--- pixqrcodegen/pixqrcodegen.py
+++ pixqrcodegen/pixqrcodegen.py
@@ -20,32 +20,24 @@ class Payload():
         self.cidade_tam = len(self.cidade)
         self.txtId_tam = len(self.txtId)
 
-        self.merchantAccount_tam = f'0014BR.GOV.BCB.PIX01{self.chavepix_tam}{self.chavepix}'
-        if self.valor_tam <= 9:
-            self.transactionAmount_tam = f'0{self.valor_tam}{self.valor}'
-        else:
-            self.transactionAmount_tam = f'{self.valor_tam}{self.valor}'
+        self.merchantAccount_tam = f'0014BR.GOV.BCB.PIX01{self.chavepix_tam:02}{self.chavepix}'
+        self.transactionAmount_tam = f'{self.valor_tam:02}{self.valor}'
 
-        if self.txtId_tam <= 9:
-            self.addDataField_tam = f'050{self.txtId_tam}{self.txtId}'
-        else:
-            self.addDataField_tam = f'05{self.txtId_tam}{self.txtId}'
+        self.addDataField_tam = f'05{self.txtId_tam:02}{self.txtId}'
 
-        if self.nome_tam <= 9:
-            self.nome_tam = f'0{self.nome_tam}'
+        self.nome_tam = f'{self.nome_tam:02}'
 
-        if self.cidade_tam <= 9:
-            self.cidade_tam = f'0{self.cidade_tam}'
+        self.cidade_tam = f'{self.cidade_tam:02}'
 
         self.payloadFormat = '000201'
-        self.merchantAccount = f'26{len(self.merchantAccount_tam)}{self.merchantAccount_tam}'
+        self.merchantAccount = f'26{len(self.merchantAccount_tam):02}{self.merchantAccount_tam}'
         self.merchantCategCode = '52040000'
         self.transactionCurrency = '5303986'
         self.transactionAmount = f'54{self.transactionAmount_tam}'
         self.countryCode = '5802BR'
-        self.merchantName = f'59{self.nome_tam}{self.nome}'
-        self.merchantCity = f'60{self.cidade_tam}{self.cidade}'
-        self.addDataField = f'62{len(self.addDataField_tam)}{self.addDataField_tam}'
+        self.merchantName = f'59{self.nome_tam:02}{self.nome}'
+        self.merchantCity = f'60{self.cidade_tam:02}{self.cidade}'
+        self.addDataField = f'62{len(self.addDataField_tam):02}{self.addDataField_tam}'
         self.crc16 = '6304'
 
   
